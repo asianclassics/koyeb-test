e73e62237c2524bc879aa54314bd947927ce82bc
joelcrawford <joelrootcrawford@gmail.com>
2021-02-22 13:34:47 -0800

    testing koyeb stack with koyeb api called from python

--- koyeb-test/__init__.py
+++ koyeb-test/__init__.py
@@ -2,4 +2,5 @@ def handler(event, context):
     print("Got YOYO data", event)
     print("Got YO context", context)
     print("Got YO context event", context.event)
+    print("MEOW")
     return {"response": "ok"}
